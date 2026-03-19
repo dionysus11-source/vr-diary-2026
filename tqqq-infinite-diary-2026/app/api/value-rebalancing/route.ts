64ce31541fb905bc2836618c3a420f6ac445101e
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2026-03-19 10:44:55 +0900

    Fix build error

--- tqqq-infinite-diary-2026/app/api/value-rebalancing/route.ts
+++ tqqq-infinite-diary-2026/app/api/value-rebalancing/route.ts
@@ -16,7 +16,7 @@ export async function GET(request: Request) {
     initialPortfolio,
     records,
     rounds,
-    transactions: rounds.flatMap(r => r.transactions)
+    transactions: rounds.flatMap((r: any) => r.transactions)
   })
 }
 
